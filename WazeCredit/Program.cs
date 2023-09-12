355af13256077eae1681da13412acff89728bee7
Anand-wase <anandvardhan.16pav@gmail.com>
2023-09-12 17:06:03 +0530

    clean program.cs file

--- WazeCredit/Program.cs
+++ WazeCredit/Program.cs
@@ -15,50 +15,13 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-builder.Services.AddTransient<IMarketForecaster, MarketForecasterV2>();
-//services.AddSingleton<IMarketForecaster>(new MarketForecasterV2());
-//services.AddTransient<MarketForecasterV2>();
-//services.AddSingleton(new MarketForecasterV2());
-//services.AddTransient(typeof(MarketForecasterV2));
-//services.AddTransient(typeof(IMarketForecaster), typeof(MarketForecasterV2));
 
-builder.Services.TryAddTransient<IMarketForecaster, MarketForecaster>();
-
-builder.Services.AddAppSettingsConfig(builder.Configuration);
+builder.Services.AddAppSettingsConfig(builder.Configuration).AddAllServices();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services.AddTransient<TransientService>();
-builder.Services.AddScoped<ScopedService>();
-builder.Services.AddSingleton<SingletonService>();
-
-builder.Services.AddScoped<CreditApprovedHigh>();
-builder.Services.AddScoped<CreditApprovedLow>();
-
-builder.Services.AddScoped<Func<CreditApprovedEnum, ICreditApproved>>(ServiceProvider => range =>
-{
-    switch (range)
-    {
-        case CreditApprovedEnum.High: return ServiceProvider.GetService<CreditApprovedHigh>();
-        case CreditApprovedEnum.Low: return ServiceProvider.GetService<CreditApprovedLow>();
-        default: return ServiceProvider.GetService<CreditApprovedLow>();
-    }
-});
-
-//services.AddScoped<IValidationChecker, AddressValidationChecker>();
-//services.AddScoped<IValidationChecker, CreditValidationChecker>();
-//services.TryAddEnumerable(ServiceDescriptor.Scoped<IValidationChecker, AddressValidationChecker>());
-//services.TryAddEnumerable(ServiceDescriptor.Scoped<IValidationChecker, CreditValidationChecker>());
-
-builder.Services.TryAddEnumerable(new[] {
-            ServiceDescriptor.Scoped<IValidationChecker, AddressValidationChecker>(),
-            ServiceDescriptor.Scoped<IValidationChecker, CreditValidationChecker>()
-            });
-
-builder.Services.AddScoped<ICreditValidator, CreditValidator>();
 
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
